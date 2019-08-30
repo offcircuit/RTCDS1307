bd90dea1bc7abac318646825914f2b59af1506ff
Helder Luciano da Costa Rodrigues <35690030+offcircuit@users.noreply.github.com>
2019-08-30 11:25:52 +0100

    Update RTCDS1307.h

--- RTCDS1307.h
+++ RTCDS1307.h
@@ -7,7 +7,9 @@
 
 class RTCDS1307 {
   private:
-    uint16_t _offset;
+    uint16_t _offset, _year;
+    uint8_t _month, _weekday, _day, _hour, _minute, _second;
+    bool _split, _period;
 
     uint8_t bcd(uint8_t data) {
       return data + 6 * (data / 10);;
@@ -17,95 +19,81 @@ class RTCDS1307 {
       return data - 6 * (data >> 4);
     }
 
-    bool isLeapYear(uint16_t year) {
-      return !((year % 4) * (!(year % 100) + (year % 400)));
-    }
-
-    uint8_t wday(uint16_t year, uint8_t month, uint8_t day) {
-      uint8_t n[12] = {31, 28 + isLeapYear(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-      year = (day + (((year - 1) * 365UL) + ((year - 1) / 4) - ((year - 1) / 100) + ((year - 1) / 400))) % 7;
-      while (month > 1) year += n[--month - 1];
-      return (year % 7) + 1;
-    }
-
   public:
+    const uint16_t &year = _year;
+    const uint8_t &month = _month, &weekday = _weekday, &day = _day, &hour = _hour, &minute = _minute, &second = _second;
+    const bool &split = _split, &period = _period;
+
     explicit RTCDS1307(uint16_t offset = 2000): _offset(offset) {
       Wire.begin();
     };
 
-    uint8_t read(uint16_t &year, uint8_t &month, uint8_t &day, uint8_t &hour, uint8_t &minute, uint8_t &second, bool &split, bool &period) {
+    bool cycle(bool state) {
+      if ((read()) && (state ^ _split)) {
+        if (state) {
+          _hour -= (_period = (_hour > 11)) * 12;
+          _hour += (_hour == 0) * 12;
+        } else _hour = (_hour % 12) + (_period * 12);
+        return write(_year, _month, _day, _hour, _minute, _second, state, _period);
+      }
+      return false;
+    }
+
+    bool isLeapYear(uint16_t Y) {
+      return !((Y % 4) * (!(Y % 100) + (Y % 400)));
+    }
+
+    bool read() {
       Wire.beginTransmission(RTCADDRESS);
       Wire.write(0);
       Wire.endTransmission();
 
       Wire.requestFrom(RTCADDRESS, 7);
 
-      second = decimal(Wire.read());
-      minute = decimal(Wire.read());
+      _second = decimal(Wire.read());
+      _minute = decimal(Wire.read());
 
       uint8_t data = Wire.read();
-      split = bitRead(data, 6);
-      period = split & bitRead(data, 5);
-      hour = decimal(data & (split ? 0x1F : 0x3F));
+      _split = bitRead(data, 6);
+      _period = split & bitRead(data, 5);
+      _hour = decimal(data & (_split ? 0x1F : 0x3F));
 
-      uint8_t weekday = decimal(Wire.read());
-      day = decimal(Wire.read());
-      month = decimal(Wire.read());
-      year = decimal(Wire.read()) + _offset;
-      return weekday;
+      _weekday = decimal(Wire.read());
+      _day = decimal(Wire.read());
+      _month = decimal(Wire.read());
+      _year = decimal(Wire.read()) + _offset;
+      return true;
     }
 
-    uint8_t write(uint16_t year, uint8_t month, uint8_t day, uint8_t hour, uint8_t minute, uint8_t second, bool split = false, bool period = false) {
-      if (second == min(uint8_t(59), second))
-        if (minute == min(uint8_t(59), minute)) {
-          if (hour == constrain(hour, split, uint8_t(split ? 12 : 23))) {
-
-            uint8_t n[12] = {31, 28 + isLeapYear(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-            if (day == constrain(day, 1, n[month - 1]))
-              if (month == constrain(month, 1, uint8_t(12))) {
-                uint8_t weekday = wday(year, month, day);
-                year = year - _offset;
-
-                if (year == min(uint8_t(99), uint8_t(year))) {
-                  Wire.beginTransmission(RTCADDRESS);
-                  Wire.write(0x00);
-                  Wire.write(bcd(second));
-                  Wire.write(bcd(minute));
-                  Wire.write(bcd(hour) | (split << 6) | ((split & period) << 5));
-                  Wire.write(bcd(weekday));
-                  Wire.write(bcd(day));
-                  Wire.write(bcd(month));
-                  Wire.write(bcd(year));
-                  Wire.endTransmission();
-                  return weekday;
-                }
-              }
-          }
-        }
-      return false;
+    uint8_t wday(uint16_t Y, uint8_t M, uint8_t D) {
+      uint8_t n[12] = {31, 28 + isLeapYear(Y), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+      Y = (D + (((Y - 1) * 365UL) + ((Y - 1) / 4) - ((Y - 1) / 100) + ((Y - 1) / 400))) % 7;
+      while (M > 1) Y += n[--M - 1];
+      return (Y % 7) + 1;
     }
 
-    uint8_t split(bool state) {
-      uint16_t year;
-      uint8_t month, weekday, day, hour, minute, second;
-      bool split, period;
+    bool write(uint16_t Y, uint8_t M, uint8_t D, uint8_t h, uint8_t m, uint8_t s, bool split = false, bool period = false) {
+      if ((s == min(uint8_t(59), s)) && (m == min(uint8_t(59), m)) && (h == constrain(h, split, uint8_t(split ? 12 : 23)))) {
+        uint8_t n[12] = {31, 28 + isLeapYear(Y), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-      if (read(year, month, day, hour, minute, second, split, period)) {
+        if ((D == constrain(D, 1, n[M - 1])) && (M == constrain(M, 1, uint8_t(12)))) {
+          uint8_t WD = wday(Y, M, D);
+          Y -= _offset;
 
-        if (state ^ split) {
-
-          if (state) {
-            period = hour > 11;
-            hour -= period * 12;
-            hour += (hour == 0) * 12;
-
-          } else {
-            hour = (hour % 12) + (period * 12);
+          if (Y == min(uint8_t(99), uint8_t(Y))) {
+            Wire.beginTransmission(RTCADDRESS);
+            Wire.write(0x00);
+            Wire.write(bcd(_second = s));
+            Wire.write(bcd(_minute = m));
+            Wire.write(bcd(_hour = h) | ((_split = split) << 6) | ((split & (_period = period)) << 5));
+            Wire.write(bcd(_weekday = WD));
+            Wire.write(bcd(_day = D));
+            Wire.write(bcd(_month = M));
+            Wire.write(bcd(_year = Y));
+            Wire.endTransmission();
+            return true;
           }
         }
-
-        return write(year, month, day, hour, minute, second, state, period);
       }
       return false;
     }
