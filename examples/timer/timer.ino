192d90acbe19b104e7c8dda4270f4ece28d2986e
Helder Luciano da Costa Rodrigues <35690030+offcircuit@users.noreply.github.com>
2019-09-03 20:51:36 +0100

    Update timer.ino

--- examples/timer/timer.ino
+++ examples/timer/timer.ino
@@ -1,6 +1,6 @@
 #include <RTCDS1307.h>
 
-RTCDS1307 rtc;
+RTCDS1307 rtc(2000);
 
 void setup()
 {
@@ -12,7 +12,6 @@ void loop()
 {
   if (rtc.read()) {
     if (!(rtc.second % 4)) rtc.cycle(1 - rtc.split);
-    Serial.print("  Day_of_week:");
     Serial.print(rtc.weekday, DEC);
     Serial.print("  ");
     Serial.print(rtc.day, DEC);
@@ -26,8 +25,7 @@ void loop()
     Serial.print(rtc.minute, DEC);
     Serial.print(":");
     Serial.print(rtc.second, DEC);
-    Serial.print(" ");
-    Serial.print(rtc.split ? (rtc.period ? "PM" : "AM"));
+    Serial.print(rtc.split ? (rtc.period ? " PM" : " AM") : "");
   }
   delay(1000);
 }
