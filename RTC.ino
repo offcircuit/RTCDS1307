c7d227e9510c73462b72eb7e0840d1c4434ffa2f
offcircuit <35690030+offcircuit@users.noreply.github.com>
2019-10-04 01:40:29 +0100

    Add files via upload

---
+++ RTC.ino
@@ -0,0 +1,40 @@
+#include "RTCDS1307.h"
+
+RTCDS1307 rtc(0x68);
+
+uint8_t year;
+uint8_t month, weekday, day, hour, minute, second;
+bool mode = 0, period = 0;
+
+void setup()
+{
+  Serial.begin(115200);
+  rtc.begin();
+  rtc.setDate(19, 9, 7);
+  rtc.setTime(11, 59, 50);
+}
+
+void loop()
+{
+  rtc.getDate(year, month, day, weekday);
+  rtc.getTime(hour, minute, second, period);
+  if (!(second % 3)) rtc.setMode(1 - rtc.getMode());
+  rtc.getTime(hour, minute, second, period);
+
+  Serial.print(weekday, DEC);
+  Serial.print("  ");
+  Serial.print(day, DEC);
+  Serial.print("/");
+  Serial.print(month, DEC);
+  Serial.print("/");
+  Serial.print(year, DEC);
+  Serial.print("  ");
+  Serial.print(hour, DEC);
+  Serial.print(":");
+  Serial.print(minute, DEC);
+  Serial.print(":");
+  Serial.print(second, DEC);
+  Serial.print(rtc.getMode() ? (period ? " PM" : " AM") : "");
+  Serial.println();
+  delay(1000);
+}
--- RTCDS1307.h
+++ RTCDS1307.h
@@ -3,17 +3,25 @@
 
 #include "Wire.h"
 
-#define RTC1307_TIMEADDR 0x00
-#define RTC1307_DATEADDR 0x03
-#define RTC1307_CONTROLADDR 0x07
-#define RTC1307_RAMADDR 0x08
+#define RTCDS1307_TIME 0x00
+#define RTCDS1307_MODE 0x02
+#define RTCDS1307_DATE 0x03
+#define RTCDS1307_CONTROL 0x07
+#define RTCDS1307_RAM 0x08
+
 
 class RTCDS1307 {
   private:
   public:
-    uint16_t _offset, _initial;
     uint8_t _address;
-    bool _mode, _period;
+
+    RTCDS1307(uint8_t address): _address(address) {};
+
+    bool begin() {
+      Wire.begin();
+      Wire.beginTransmission(_address);
+      return Wire.endTransmission(1);
+    }
 
     uint8_t bcd(uint8_t val) {
       return val + (6 * (val / 10));
@@ -34,60 +42,89 @@ class RTCDS1307 {
       return (Y % 7) + 1;
     }
 
-    RTCDS1307(uint8_t address, uint16_t offset = 0, uint16_t initial = 2000):  _address(address), _offset(offset), _initial(initial) {
-      _initial = 2000;
-    };
-
-    void begin() {
-      Wire.begin();
+    void setDate(uint8_t Y, uint8_t M, uint8_t D) {
       Wire.beginTransmission(_address);
-      return Wire.endTransmission(1);
+      Wire.write(RTCDS1307_DATE);
+      Wire.write(wday(Y, M, D));
+      Wire.write(bcd(D));
+      Wire.write(bcd(M));
+      Wire.write(bcd(Y));
+      Wire.endTransmission(1);
     }
 
-    void getDate(uint16_t &Y, uint8_t &M, uint8_t &D, uint8_t &WD) {
-      uint8_t *buffer;
-      read(RTC1307_DATEADDR, buffer, 4);
-      WD = decimal(buffer[0]);
-      D = decimal(buffer[1]);
-      M = decimal(buffer[2]);
-      Y = decimal(buffer[3]) + _initial;
-    }
+    void getDate(uint8_t &Y, uint8_t &M, uint8_t &D, uint8_t &WD) {
+      Wire.beginTransmission(_address);
+      Wire.write(RTCDS1307_DATE);
+      Wire.endTransmission(0);
 
-    void getTime(uint8_t &h, uint8_t &m, uint8_t &s, bool &mode, bool &period) {
-      uint8_t *buffer;
-      read(RTC1307_TIMEADDR, buffer, 3);
-      s = decimal(buffer[0]);
-      m = decimal(buffer[1]);
-      mode = bitRead(buffer[2], 6);
-      period = mode & bitRead(buffer[2], 5);
-      h = decimal(buffer[2] & (mode ? 0x1F : 0x3F));
-    }
-
-    void setDate(uint16_t Y, uint8_t M, uint8_t D) {
-      uint8_t data[4] = {wday(Y, M, D), bcd(D), bcd(M), bcd(Y - _initial)};
-      write(RTC1307_DATEADDR, data, 4);
+      Wire.requestFrom(_address, uint8_t(4), uint8_t(1));
+      while (Wire.available() < 4);
+      WD = Wire.read();
+      D = decimal(Wire.read());
+      M = decimal(Wire.read());
+      Y = decimal(Wire.read());
     }
 
     void setTime(uint8_t h, uint8_t m, uint8_t s, bool mode = 0, bool period = 0) {
-      uint8_t data[3] = {bcd(s), bcd(m), bcd(h) | mode << 6 | (mode & period) << 5};
-      write(RTC1307_TIMEADDR, data, 3);
-    }
-
-    bool write(uint8_t address, uint8_t *data, uint8_t length) {
       Wire.beginTransmission(_address);
-      Wire.write(address);
-      for (uint8_t i = 0; i < length; i++) Wire.write(data[i]);
-      return Wire.endTransmission(1);
-    }
-
-    bool read(uint8_t address, uint8_t *&buffer, uint8_t length) {
-      buffer = (uint8_t *) realloc(buffer, length);
-      Wire.beginTransmission(_address);
-      Wire.write(address);
+      Wire.write(RTCDS1307_TIME);
+      Wire.write(bcd(s));
+      Wire.write(bcd(m));
+      Wire.write(bcd(h) | mode << 6 | (mode & period) << 5);
       Wire.endTransmission(1);
-      Wire.requestFrom(_address, length);
-      while (Wire.available() < length);
-      for (uint8_t i = 0; i < length; i++) buffer[i] = uint8_t(Wire.read());
+    }
+
+    void getTime(uint8_t &h, uint8_t &m, uint8_t &s, bool &period) {
+      Wire.beginTransmission(_address);
+      Wire.write(RTCDS1307_TIME);
+      Wire.endTransmission(0);
+
+      Wire.requestFrom(_address, uint8_t(3), uint8_t(1));
+      while (Wire.available() < 3);
+      s = decimal(Wire.read());
+      m = decimal(Wire.read());
+
+      h = Wire.read();
+      period = bitRead(h, 6) & bitRead(h, 5);
+      h = decimal(h & (bitRead(h, 6) ? 0x1F : 0x3F));
+    }
+
+    void getTime(uint8_t &h, uint8_t &m, uint8_t &s) {
+      bool period;
+      getTime(h, m, s, period);
+    }
+
+    void setMode(bool state) {
+      Wire.beginTransmission(_address);
+      Wire.write(RTCDS1307_MODE);
+      Wire.endTransmission(0);
+
+      Wire.requestFrom(_address, uint8_t(1), uint8_t(1));
+      while (Wire.available() < 1);
+      uint8_t h = Wire.read();
+
+      if (state ^ bitRead(h, 6)) {
+        bool period = bitRead(h, 5);
+        h = decimal(h & (bitRead(h, 6) ? 0x1F : 0x3F));
+
+        if (state) h += ((h == 12) - (period = (h > 11))) * 12;
+        else h = (h % 12) + (period * 12);
+
+        Wire.beginTransmission(_address);
+        Wire.write(RTCDS1307_MODE);
+        Wire.write(bcd(h) | state << 6 | (state & period) << 5);
+        Wire.endTransmission(0);
+      }
+    }
+
+    bool getMode() {
+      Wire.beginTransmission(_address);
+      Wire.write(RTCDS1307_MODE);
+      Wire.endTransmission(0);
+
+      Wire.requestFrom(_address, uint8_t(1), uint8_t(1));
+      while (Wire.available() < 1);
+      return bitRead(Wire.read(), 6);
     }
 
 
@@ -110,40 +147,6 @@ class RTCDS1307 {
 
 
 
-
-
-
-
-
-
-    /*    bool midday(bool state) {
-          if ((read()) && (state ^ _mode)) {
-            if (state) {
-              _hour -= (_period = (_hour > 11)) * 12;
-              _hour += (_hour == 0) * 12;
-            } else _hour = (_hour % 12) + (_period * 12);
-            return write(_year, _month, _day, _hour, _minute, _second, state, _period);
-          }
-          return false;
-        }
-    */
-    bool setTimestamp(unsigned long t) {
-      uint8_t s = t % 60;
-      uint8_t m = (t /= 60) % 60;
-      uint8_t h = (t /= 60) % 24;
-      bool p = _mode * (h > 11);
-      h += (_mode * !h * 12);
-
-      uint16_t Y;
-      for (Y = 1970; t > (365 + isLeapYear(Y)); Y++) t -= (365 + isLeapYear(Y));
-
-      uint8_t M;
-      uint8_t n[12] = {31, 28 + isLeapYear(Y), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-      for (M = 1; t >= n[M - 1]; M++) t -= n[M - 1];
-
-      if (Y < _initial) return false;
-      //      else return write(Y, M, t + 1, h, m, s, _mode, p);
-    }
 };
 
 #endif
