56aa705119891a22ee8e0e50eba80604651ac090
offcircuit <35690030+offcircuit@users.noreply.github.com>
2019-10-04 16:49:37 +0100

    Update timer.ino

--- examples/timer/timer.ino
+++ examples/timer/timer.ino
@@ -1,31 +1,42 @@
 #include "RTCDS1307.h"
 
-RTCDS1307 rtc(2000);
+RTCDS1307 rtc(0x68);
+
+uint8_t year, month, weekday, day, hour, minute, second;
+bool period = 0;
+
+String m[12] = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+String w[7] = {"Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"};
 
 void setup()
 {
   Serial.begin(115200);
-  rtc.write(2019, 8, 29, 23, 59, 54);
+  rtc.begin();
+  rtc.setDate(19, 2, 28);
+  rtc.setTime(23, 59, 50);
 }
 
 void loop()
 {
-  if (rtc.read()) {
-    if (!(rtc.second % 4)) rtc.midday(1 - rtc.mode);
-    Serial.print(rtc.weekday, DEC);
-    Serial.print("  ");
-    Serial.print(rtc.day, DEC);
-    Serial.print("/");
-    Serial.print(rtc.month, DEC);
-    Serial.print("/");
-    Serial.print(rtc.year, DEC);
-    Serial.print("  ");
-    Serial.print(rtc.hour, DEC);
-    Serial.print(":");
-    Serial.print(rtc.minute, DEC);
-    Serial.print(":");
-    Serial.print(rtc.second, DEC);
-    Serial.println(rtc.mode ? (rtc.period ? " PM" : " AM") : "");
-  }
+  rtc.getDate(year, month, day, weekday);
+  rtc.getTime(hour, minute, second, period);
+  if (!(second % 3)) rtc.setMode(1 - rtc.getMode());
+  rtc.getTime(hour, minute, second, period);
+
+  Serial.print(w[weekday - 1]);
+  Serial.print("  ");
+  Serial.print(day, DEC);
+  Serial.print("/");
+  Serial.print(m[month]);
+  Serial.print("/");
+  Serial.print(year + 2000, DEC);
+  Serial.print("  ");
+  Serial.print(hour, DEC);
+  Serial.print(":");
+  Serial.print(minute, DEC);
+  Serial.print(":");
+  Serial.print(second, DEC);
+  Serial.print(rtc.getMode() ? (period ? " PM" : " AM") : "");
+  Serial.println();
   delay(1000);
 }
